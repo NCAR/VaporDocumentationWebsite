275946ee4ca687ea4beb76db9515c28820220951
sgpearse <scottgpearse@gmail.com>
2025-05-27 08:11:08 -0600

    test

--- docs/conf.py
+++ docs/conf.py
@@ -113,8 +113,8 @@ language = 'en'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_static_path = ["_static"]
-html_css_files = ["custom.css"]
+#html_static_path = ["_static"]
+#html_css_files = ["custom.css"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
